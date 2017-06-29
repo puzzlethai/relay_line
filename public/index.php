c54619989912c0f5b289e7cb859c4b8f840bb326
OZONE <iq_maze@yahoo.com>
2017-06-29 12:00:18 +0700

    ver 24 pass token in var of get  profile

--- public/index.php
+++ public/index.php
@@ -19,6 +19,10 @@ echo 'test echo';
 $client_id = "1519057505";
 $client_secret = "5997cf65a3c3789378fa99526d0f1b8c";
 $redirect_uri = "https%3A%2F%2Frelayline.herokuapp.com";
+global $ozone ="first";
+echo $ozone;
+exit();
+
 // $token = "";
 echo 'echo 2   ';
 function getToken($code){
